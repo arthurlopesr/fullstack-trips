6aec12a5005ffb6f45448d5d24fbfcc714879065
Arthur Lopes <arthurlopr12@gmail.com>
2023-07-18 11:27:56 -0300

    chore: add next auuth prisma cinfig

--- src/app/about/components/trips/Trips.tsx
+++ src/app/about/components/trips/Trips.tsx
@@ -1,9 +1,24 @@
+import { prisma } from "@/lib/prisma";
 import React from "react";
 
-const Trips = () => {
+const getTrips = async () => {
+  const trips = await prisma.trip.findMany({});
+
+  return trips;
+}
+
+const Trips = async () => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    next: {
+      revalidate: false
+    }
+  }).then((res) => res.json());
+
   return (
     <div>
-      Trips
+      {data.map((i: any) => {
+        <p key={i.id}>{i.title}</p>
+      })}
     </div>
   );
 };
