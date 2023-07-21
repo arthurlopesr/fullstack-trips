4429958eaf599924295207e415018ed28f427cbb
Arthur Lopes <arthurlopr12@gmail.com>
2023-07-21 10:36:53 -0300

    feat: add validation to trip reservation

--- src/app/trips/[tripId]/components/tripReservation/TripReservation.tsx
+++ src/app/trips/[tripId]/components/tripReservation/TripReservation.tsx
@@ -1,35 +1,95 @@
 "use client";
-
+import React from "react";
 import Button from "@/components/button/Button";
 import DatePicker from "@/components/datePicker/DatePicker";
 import Input from "@/components/input/Input";
 import { Trip } from "@prisma/client";
 import { TripDescription } from "../tripDescription/TripDescription";
+import { Controller, useForm } from "react-hook-form";
 
 
 interface TripReservationProps {
-  trip: Trip
+  trip: Trip;
+}
+
+interface TripReservartionForm {
+  guests: number;
+  startDate: Date | null;
+  endDate: Date | null;
 }
 
 export function TripReservation({ trip }: TripReservationProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control
+  } = useForm<TripReservartionForm>();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
 
     <div className="flex flex-col px-5 pb-10">
-
       <div className="flex gap-4">
-        <DatePicker
-          className="w-full"
-          placeholderText="Data Inicial"
-          onChange={() => { }}
+        <Controller
+          name="startDate"
+          rules={{
+            required: {
+              value: true,
+              message: "Data inicial é obrigatória"
+            }
+          }}
+          control={control}
+          render={({ field }) => (
+            <DatePicker
+            className="w-full"
+            placeholderText="Data Inicial"
+            onChange={field.onChange}
+            selected={field.value}
+            error={!!errors?.startDate}
+            errorMessage={errors?.startDate?.message}
+          />
+          )}
         />
-        <DatePicker
-          className="w-full"
-          placeholderText="Data Final"
-          onChange={() => { }}
+
+        <Controller
+          name="endDate"
+          rules={{
+            required: {
+              value: true,
+              message: "Data final é obrigatória"
+            }
+          }}
+          control={control}
+          render={({ field }) => (
+            <DatePicker
+            className="w-full"
+            placeholderText="Data Final"
+            onChange={field.onChange}
+            selected={field.value}
+            error={!!errors?.endDate}
+            errorMessage={errors?.endDate?.message}
+          />
+          )}
         />
+
       </div>
 
-      <Input className="mt-4" placeholder={`Número de hóspedes (max: ${trip.maxGuests})`} />
+      <Input
+        className="mt-4"
+        placeholder={`Número de hóspedes (max: ${trip.maxGuests})`}
+        {...register('guests', {
+          required: {
+            value: true,
+            message: "Número de hóspedes obrigatório."
+          }
+        })}
+        error={!!errors?.guests}
+        errorMessage={errors?.guests?.message}
+      />
 
       <div className="flex justify-between mt-3">
         <p className="font-medium text-sm text-primaryDarker">Total: </p>
@@ -37,12 +97,15 @@ export function TripReservation({ trip }: TripReservationProps) {
       </div>
 
       <div className="pb-10 border-b border-grayLighter w-full">
-        <Button className="mt-3 w-full">
+
+        <Button className="mt-3 w-full" onClick={() => handleSubmit(onSubmit)()}>
           Reservar agora
         </Button>
+
       </div>
 
-      <TripDescription description={trip.description}/>
+      <TripDescription description={trip.description} />
+
     </div>
   )
 }
