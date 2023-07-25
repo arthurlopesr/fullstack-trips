69828dca3999cccc4626eea4c2e82fbd7d2d6b1b
Arthur Lopes <arthurlopr12@gmail.com>
2023-07-24 22:03:49 -0300

    feat: add trip search

--- src/app/components/tripSearch/TripSearch.tsx
+++ src/app/components/tripSearch/TripSearch.tsx
@@ -4,29 +4,77 @@ import Button from "@/components/button/Button";
 import CurrencyInput from "@/components/currencyInput/CurrencyInput";
 import DatePicker from "@/components/datePicker/DatePicker";
 import Input from "@/components/input/Input";
+import { useRouter } from "next/navigation";
+import { Controller, useForm } from "react-hook-form";
 
+interface TripSearchForm {
+  text: string;
+  startDate: Date | null;
+  budget: string;
+}
 
-export function TripSearch() {
+export function TripSearch(data: TripSearchForm) {
+  const router = useRouter();
+
+  const {
+    control,
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm<TripSearchForm>();
+
+  const onSubmit = (data: TripSearchForm) => {
+    router.push(`/trips/search?text=${data.text}&startDate=${data.startDate?.toISOString()}&budget=${data.budget}`);
+  };
+
   return (
-    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat">
-      <h1 className="font-semibold text-2xl text-primaryDarker text-center">
+    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat lg:py-28">
+      <h1 className="font-semibold text-2xl text-primaryDarker text-center lg:text-[2.5rem]">
         Encontre sua próxima <span className="text-primary">viagem!</span>
       </h1>
 
-      <div className="flex flex-col gap-4 mt-5">
-        <Input placeholder="Onde você quer ir?" />
+      <div className="flex flex-col gap-4 mt-5 lg:flex-row lg:max-w-[948px] lg:mx-auto lg:p-4 lg:bg-primary lg:mt-12 lg:bg-opacity-20 lg:rounded-lg">
+        <Input
+          placeholder="Onde você quer ir?"
+          error={!!errors.text}
+          errorMessage={errors.text?.message}
+          {...register("text", {
+            required: {
+              value: true,
+              message: "Texto é obrigatório.",
+            },
+          })}
+        />
 
-        <div className="flex gap-4">
-          <DatePicker
-            placeholderText="Data de ida"
-            onChange={() => {}}
-            className="w-full"
-            />
-          <CurrencyInput placeholder="Orçamento?"/>
+        <div className="flex gap-4 lg:w-full">
+          <Controller
+            name="startDate"
+            control={control}
+            render={({ field }) => (
+              <DatePicker onChange={field.onChange} selected={field.value} placeholderText="Data Final" className="w-full" minDate={new Date()} />
+            )}
+          />
+
+          <Controller
+            name="budget"
+            control={control}
+            render={({ field }) => (
+              <CurrencyInput
+                allowDecimals={false}
+                placeholder="Orçamento"
+                onValueChange={field.onChange as any}
+                value={field.value}
+                onBlur={field.onBlur}
+              />
+            )}
+          />
         </div>
 
-        <Button>Buscar</Button>
+        <Button onClick={() => handleSubmit(onSubmit)()} className="w-1/2 lg:h-fit">
+          Buscar
+        </Button>
       </div>
     </div>
-  )
-}
+  );
+};
+
